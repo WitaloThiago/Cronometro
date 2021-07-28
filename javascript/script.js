010e501c3e5d8e7369595c59ce72bb6198b8ca00
Witalo Thiago <thiagowitalo86@gmail.com>
2021-07-28 13:09:04 -0300

    final

---
+++ javascript/script.js
@@ -0,0 +1,46 @@
+var hh = 0
+var mm = 0
+var ss = 0
+var tempo = 1000
+
+var clean
+
+function iniciar() {
+
+    clean = setInterval(()=> start(), tempo)
+}
+
+function zerar() {
+    clearInterval(clean)
+
+    document.getElementById("resultado").innerHTML = "00:00:00"
+
+}
+
+function pausar(){
+
+    clearInterval(clean)
+
+}
+
+
+function start() {
+
+    ss++;
+    if (ss == 60) {
+        ss = 0;
+        mm++;
+
+        if (mm == 60) {
+            mm = 0;
+            hh++
+        }
+    }
+
+
+    const final = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss)
+
+    document.getElementById("resultado").innerHTML = final
+
+
+}
